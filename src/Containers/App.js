b395f2e85e5a2dd829a8ef588bc5056743417310
ValentinMarturet <valen.martu2206@gmail.com>
2021-11-03 18:49:28 -0300

    First commit

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,48 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../Components/CardList';
 import SearchBox from '../Components/SearchBox';
 import Scroll from '../Components/Scroll';
 import './App.css';
 import ErrorBoundry from '../Components/ErrorBoundry';
 
-class App extends React.Component {
-	constructor() {
-		super()
-		this.state = {
-			robots: [],
-			searchfield: ''
-		}
-	}
+import { setSearchField } from '../actions.js'
 
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response => response.json())
-		.then(users => this.setState({ robots: users }));
-	}
-
-// onSearchChange es un evento que cambia el valor del state y lo actualiza
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value })
-	}
-	render() {
-		const { robots, searchfield } = this.state;
-		const filteredRobots = robots.filter(robot =>{
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-		})
-		return !robots.length ?
-			<h1 className='tc'>Loading</h1> :
-			(
-				<div className='tc'>
-					<h1 className='f1'>RoboFriends</h1>
-					<SearchBox searchChange={this.onSearchChange}/>
-					<Scroll>
-						<ErrorBoundry>
-							<CardList robots={filteredRobots}/>
-						</ErrorBoundry>
-					</Scroll>
-				</div>
-			);
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchField
 	}
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
+
+function App(props) {
+	// constructor() {
+	// 	super()
+	// 	this.state = {
+	// 		robots: [],
+	// 		searchfield: ''
+	// 	}
+	// }
+	const [robots, setRobots] = useState([]);
+	// const [searchfield, setSearchfield] = useState('');
+
+	// componentDidMount() {
+	// 	fetch('https://jsonplaceholder.typicode.com/users')
+	// 	.then(response => response.json())
+	// 	.then(users => this.setState({ robots: users }));
+	// }
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => setRobots(users));
+	}, [])
+// onSearchChange es un evento que cambia el valor del state y lo actualiza
+	// const onSearchChange = (event) => {
+	// 	setSearchfield(event.target.value)
+	// }
+	
+	const { searchField, onSearchChange } = props;
+
+	const filteredRobots = robots.filter(robot =>{
+		return robot.name.toLowerCase().includes(searchField.toLowerCase());
+	})
+	return !robots.length ?
+		<h1 className='tc'>Loading</h1> :
+		(
+			<div className='tc'>
+				<h1 className='f1'>RoboFriends</h1>
+				<SearchBox searchChange={onSearchChange}/>
+				<Scroll>
+					<ErrorBoundry>
+						<CardList robots={filteredRobots}/>
+					</ErrorBoundry>
+				</Scroll>
+			</div>
+		);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './Containers/App';
 import reportWebVitals from './reportWebVitals';
+import { searchRobots } from './reducers.js';
 import 'tachyons';
 
-ReactDOM.render(<App/>
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
   ,document.getElementById('root'));
 
 
